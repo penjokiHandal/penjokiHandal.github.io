6c6ed63e413574747ffab51c91152589ef3d8294
Muhamadfaruqosama <abdolslamet@gmail.com>
2024-01-06 14:33:36 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,20 @@ setTimeout(function() {
         document.getElementById('logo-penjoki-handal').classList.add('hidden');
         document.getElementById('welcome').classList.add('hidden');
         document.getElementById('loader-page').classList.add('hidden');
+        setTimeout(function() {
+            document.getElementById('loader-page').style.display = "none";
+        }, 1000)
     }, 1000)
 },800)
+
+function showSidebar() {
+    const hamburger = document.querySelector('.fa-bars');
+    const xMark = document.querySelector('.fa-xmark');
+    if(hamburger != '') {
+        hamburger.classList.remove('fa-bars');
+        hamburger.classList.add('fa-xmark');
+    } else {
+        xMark.classList.remove('fa-xmark');
+        xMark.classList.add('fa-bars');
+    }
+}
